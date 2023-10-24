7c60404fb8006be938c1c49d99c6541085cbac2e
Nikasan1 <nikasan4@gmail.com>
2023-10-24 13:54:52 +0200

    Second commit

--- Src/homework13.java
+++ Src/homework13.java
@@ -3,16 +3,16 @@ import java.util.Scanner;
 
 public class homework13 {
     public static void main(String[] args) {
-        Scanner scanner= new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Введіть розміри матриці :");
         int lengthArrayNumA = scanner.nextInt();
         int lengthArrayNumB = scanner.nextInt();
-        int [][] array= new int [lengthArrayNumA][lengthArrayNumB];
+        int[][] array = new int[lengthArrayNumA][lengthArrayNumB];
         Random random = new Random();
         System.out.println("Матриця");
-        for (int i=0; i<lengthArrayNumA;i++){
-            for (int j=0;j<lengthArrayNumB; j++){
-                array[i][j]= random.nextInt(10);
+        for (int i = 0; i < lengthArrayNumA; i++) {
+            for (int j = 0; j < lengthArrayNumB; j++) {
+                array[i][j] = random.nextInt(10);
             }
         }
         for (int i = 0; i < lengthArrayNumA; i++) {
@@ -23,11 +23,11 @@ public class homework13 {
         }
         System.out.println("Транспонована матриця");
 
-        for (int i=0; i<lengthArrayNumA;i++){
-            for (int j=i+1;j<lengthArrayNumB; j++){
-                int temp =array[i][j];
-                array[i][j]=array[j][i];
-                array[j][i]= temp;
+        for (int i = 0; i < lengthArrayNumA; i++) {
+            for (int j = i + 1; j < lengthArrayNumB; j++) {
+                int temp = array[i][j];
+                array[i][j] = array[j][i];
+                array[j][i] = temp;
             }
         }
         for (int i = 0; i < lengthArrayNumA; i++) {
